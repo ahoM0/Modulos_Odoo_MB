e8d130ba7df69ba8bad80fa1f9092f4bbbdc1892
ahoM0 <dev.mohaben@gmail.com>
2024-02-06 16:32:30 +0000

    Tarea001-MB Hecha

--- ej02-lista_tareas/models/models.py
+++ ej02-lista_tareas/models/models.py
@@ -18,7 +18,9 @@ class lista_tareas(models.Model):
    
     tarea = fields.Char()
     prioridad = fields.Integer()
+    fecha = fields.Date()
 
+    fecha_fin = fields.Date()
     #Indicamos que este valor es computado y se computara con la funcion "_value_urgente"
     #Con store=True indicamos que pese a ser computado, cada vez que se compute se guarde en la base de datos
     #esto se hace para que podamos utilizar el campo en busquedas, filtrados y ordenaciones
